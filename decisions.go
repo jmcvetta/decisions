02a7ca0d4f736598aab139d60d128589caa70f47
Jason McVetta <jason.mcvetta@gmail.com>
2012-11-21 03:20:02 -0800

    Better validation, and log to database

--- decisions.go
+++ decisions.go
@@ -5,20 +5,32 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"github.com/bmizerany/pat"
 	"github.com/darkhelmet/env"
 	"github.com/jmcvetta/randutil"
 	"io/ioutil"
+	"labix.org/v2/mgo"
 	"log"
 	"net/http"
+	// "labix.org/v2/mgo/bson"
+
 )
 
-type payload struct {
-	Choices []string
+var db *mgo.Database
+
+type DecisionRequest struct {
+	Quandary string
+	Choices  []string
 }
 
-type decision struct {
-	Choice string
+type DecisionResponse struct {
+	Winner string
+}
+
+type Decision struct {
+	DecisionRequest
+	DecisionResponse
 }
 
 // Decide receives a JSON payload containing several strings, and returns a JSON
@@ -37,25 +49,58 @@ func Decide(w http.ResponseWriter, req *http.Request) {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	var pl payload
-	err = json.Unmarshal(body, &pl)
-	log.Println(pl)
+	var dreq DecisionRequest
+	err = json.Unmarshal(body, &dreq)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	choice, err := randutil.ChoiceString(pl.Choices)
+	//
+	// Discard empty choices
+	//
+	validChoices := []string{}
+	for _, choice := range dreq.Choices {
+		if choice != "" {
+			validChoices = append(validChoices, choice)
+		}
+	}
+	if len(validChoices) < 1 {
+		msg := fmt.Sprintln("Must provide at least 2 choices, but you provided", len(validChoices))
+		log.Println(msg)
+		// msg := "Must provide at least 2 choices"
+		http.Error(w, msg, http.StatusBadRequest)
+		return
+	}
+	//
+	// Make the decision
+	//
+	winner, err := randutil.ChoiceString(validChoices)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
-	bytes, err := json.Marshal(decision{choice})
+	dres := DecisionResponse{winner}
+	//
+	// Save to Database
+	//
+	c := db.C("quandaries")
+	d := Decision{dreq, dres}
+	err = c.Insert(&d)
+	if err != nil {
+		msg := "MongoDB Error: " + err.Error()
+		http.Error(w, msg, http.StatusInternalServerError)
+		return
+	}
+	//
+	// Generate response
+	//
+	blob, err := json.Marshal(dres)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	req.Header.Add("content-type", "application/json")
-	w.Write(bytes)
+	w.Write(blob)
 }
 
 func main() {
@@ -65,18 +110,29 @@ func main() {
 	//
 	port := env.StringDefault("PORT", "9000")
 	pwd := env.StringDefault("PWD", "/app")
+	mongoUrl := env.StringDefault("MONGOLAB_URI", "localhost")
+	//
+	// Connect to MongoDB
+	//
+	log.Println("Connecting to MongoDB on " + mongoUrl + "...")
+	session, err := mgo.Dial(mongoUrl)
+	if err != nil {
+		log.Panicln(err)
+	}
+	defer session.Close()
+	db = session.DB("")
 	//
 	// Routing
 	//
 	mux := pat.New()
 	mux.Post("/decide", http.HandlerFunc(Decide))
 	http.Handle("/v1/", http.StripPrefix("/v1", mux))
-	http.Handle("/", http.FileServer(http.Dir(pwd + "/angular/app")))
+	http.Handle("/", http.FileServer(http.Dir(pwd+"/angular/app")))
 	//
 	// Start Webserver
 	//
 	log.Println("Starting webserver on port " + port + "...")
-	err := http.ListenAndServe(":"+port, nil)
+	err = http.ListenAndServe(":"+port, nil)
 	if err != nil {
 		log.Panicln(err)
 	}
